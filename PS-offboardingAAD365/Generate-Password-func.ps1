2eda2fa9e485c7f0deee8e812700747a4486588e
D3jag0re <mbrinkwo@gmail.com>
2023-04-14 08:53:26 -0400

    implemented pw gen

--- PS-offboardingAAD365/Generate-Password-func.ps1
+++ PS-offboardingAAD365/Generate-Password-func.ps1
@@ -1,40 +1,26 @@
-$uppercase = [char[]]('A'..'Z')
-$lowercase = [char[]]('a'..'z')
-$numbers = [char[]]('0'..'9')
-$specialChars = [char[]]('!','@','#','$','%','^','&','*','(',')','-','_','=','+','[',']','{','}',';',':','<','>',',','.','?','/','\','|','~','`')
-
-$validChars = $uppercase + $lowercase + $numbers + $specialChars
-$password = ''
-$rand = New-Object System.Random
-
-while ($password.Length -lt 12) {
-    $nextChar = $validChars[$rand.Next(0, $validChars.Length)]
-    if ($nextChar -in $uppercase) {
-        if (-not $uppercaseUsed) {
-            $uppercaseUsed = $true
+function Generate-Password {
+    param(
+        [int]$Length = 12,
+        [int]$Count = 1
+    )
+    
+    $UpperCaseChars = [char[]]([char]'A'..[char]'Z')
+    $LowerCaseChars = [char[]]([char]'a'..[char]'z')
+    $NumberChars = [char[]]([char]'0'..[char]'9')
+    $SpecialChars = [char[]]('!', '@', '#', '$', '%', '^', '&', '*','|', ';', ':', ',', '.', '/', '?')
+    
+    for ($i = 1; $i -le $Count; $i++) {
+        $PasswordChars = @()
+        for ($j = 1; $j -le $Length; $j++) {
+            switch (Get-Random -Minimum 1 -Maximum 5) {
+                1 { $PasswordChars += $UpperCaseChars | Get-Random }
+                2 { $PasswordChars += $LowerCaseChars | Get-Random }
+                3 { $PasswordChars += $NumberChars | Get-Random }
+                4 { $PasswordChars += $SpecialChars | Get-Random }
+            }
         }
-    } elseif ($nextChar -in $lowercase) {
-        if (-not $lowercaseUsed) {
-            $lowercaseUsed = $true
-        }
-    } elseif ($nextChar -in $numbers) {
-        if (-not $numberUsed) {
-            $numberUsed = $true
-        }
-    } elseif ($nextChar -in $specialChars) {
-        if (-not $specialCharUsed) {
-            $specialCharUsed = $true
-        }
+        $newpass = -join $PasswordChars
+        Write-Output $newpass
     }
-
-    $password += $nextChar
 }
-
-if (-not $uppercaseUsed -or -not $lowercaseUsed -or -not $numberUsed -or -not $specialCharUsed) {
-    $password = $password.Remove($rand.Next(0, $password.Length), 1)
-    $password += $validChars[$rand.Next(0, $validChars.Length)]
-}
-
-Write-Output $password
-
-
+Generate-Password 
--- PS-offboardingAAD365/offboardingGraph-v1.ps1
+++ PS-offboardingAAD365/offboardingGraph-v1.ps1
@@ -14,8 +14,34 @@ $user.DisplayName = "x " + $user.DisplayName
 Update-MgUser -UserId $user.Id -Displayname $user.Displayname
 
 # create new password
-$newpass = [System.Web.Security.Membership]::GeneratePassword(12,1)
+function Generate-Password {
+    param(
+        [int]$Length = 12,
+        [int]$Count = 1
+    )
+    
+    $UpperCaseChars = [char[]]([char]'A'..[char]'Z')
+    $LowerCaseChars = [char[]]([char]'a'..[char]'z')
+    $NumberChars = [char[]]([char]'0'..[char]'9')
+    $SpecialChars = [char[]]('!', '@', '#', '$', '%', '^', '&', '*','|', ';', ':', ',', '.', '/', '?')
+    
+    for ($i = 1; $i -le $Count; $i++) {
+        $PasswordChars = @()
+        for ($j = 1; $j -le $Length; $j++) {
+            switch (Get-Random -Minimum 1 -Maximum 5) {
+                1 { $PasswordChars += $UpperCaseChars | Get-Random }
+                2 { $PasswordChars += $LowerCaseChars | Get-Random }
+                3 { $PasswordChars += $NumberChars | Get-Random }
+                4 { $PasswordChars += $SpecialChars | Get-Random }
+            }
+        }
+        $newpass = -join $PasswordChars
+        Write-Output $newpass
+    }
+}
+
 $newpass = ConvertTo-SecureString -String $newpass -AsPlainText -Force
 
 # Set new password
 Update-MgDUserPassword -UserId $upn -Password $newpass
+
