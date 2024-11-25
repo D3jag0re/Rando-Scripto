6b2676fe2296db20dfaa89a88b40fdf9fad08ead
Mat B <mbrinkwo@gmail.com>
2024-11-25 10:32:29 -0500

    add policy def dir

---
+++ AZ-Policy-Definitions/README.md
@@ -0,0 +1,6 @@
+# AZ Policy Definitions 
+
+JSON dump for custom definitions: 
+
+- [resourceGroupTags.json](resourceGroupTags.json) : To enforce 3 default tags required when creating a resource group. 
+
--- SH-Nginx-Log-Analyser/loganalyser.sh
+++ SH-Nginx-Log-Analyser/loganalyser.sh
@@ -2,9 +2,11 @@
 
 # Top 5 IP Addresses with the most requests
 
-awk '{print $1}' nginx-access.txt | sort | uniq -c
+awk '{print $1}' nginx-access.log | sort | uniq -c | sort -nr | head -n5
 
 # Top 5 Most Requested Paths 
+
+awk '{print $7}' nginx-access.log | sort | uniq -c | sort -nr | head -n5
 
 # Top 5 Response Status Codes
 
