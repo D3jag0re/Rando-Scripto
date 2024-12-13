b17767b53c8a0ae273f8f1b1cc0bc53a58ff4ed9
Mat B <mbrinkwo@gmail.com>
2024-12-13 14:07:06 -0500

    update

--- PS-Silent-Install/shortcutOneLiner.ps1
+++ PS-Silent-Install/shortcutOneLiner.ps1
@@ -3,11 +3,3 @@
 # Full-Size version in ./shortcut.ps1
 
 $ws = New-Object -ComObject WScript.Shell; $sc = $ws.CreateShortcut([System.IO.Path]::Combine([System.Environment]::GetFolderPath('Desktop'), 'Watchguard VPN.lnk')); $sc.TargetPath = "C:\Program Files (x86)\WatchGuard\WatchGuard Mobile VPN with SSL\wgsslvpnc"; $sc.WorkingDirectory = "C:\Program Files (x86)\WatchGuard\WatchGuard Mobile VPN with SSL\"; $sc.Save()
-
-
-
-
-
-
-
-
