78cb3359a4c42822cf16450a70c67ac3f419e8b3
Mat B <91279878+D3jag0re@users.noreply.github.com>
2025-05-15 09:31:25 -0400

    new PS script for last created users

---
+++ PS-Last-Created-Users/README.md
@@ -0,0 +1,16 @@
+# PS-Last-Created-Users
+
+This script queries an on-Prem Domain Controller for the last 20 users created, returning name, email, and date created. It then exports this information to a log in a .txt file to the location specified. 
+
+Best if run on an on-prem Domain Controller as Admin. 
+
+## TO RUN 
+
+Note: This assumes the script is being run on a Domain Controller. 
+
+1. Change the variables at the top of the script to suit your needs: 
+    - $logPath
+
+2. Run Script
+
+3. Retrieve log from path specified. 
---
+++ PS-Last-Created-Users/lastCreatedUsersLogging.ps1
@@ -0,0 +1,17 @@
+# This script 
+
+# Import the Active Directory module
+Import-Module ActiveDirectory
+
+# Define output path
+$logPath = "C:\Logs\Last20CreatedUsers.txt"
+
+# Ensure the Logs folder exists
+New-Item -ItemType Directory -Path (Split-Path $logPath) -Force | Out-Null
+
+# Get the last 20 created users and export to log
+Get-ADUser -Filter * -Properties Name, EmailAddress, whenCreated |
+    Sort-Object whenCreated -Descending |
+    Select-Object -First 20 -Property Name, EmailAddress, whenCreated |
+    Format-Table Name, EmailAddress, whenCreated -AutoSize |
+    Out-String | Set-Content -Path $logPath
